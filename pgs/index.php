60b457d0f51af6fa6c1bbb0bf60a282b1fffa10e
FredDon <gsiner@live.com>
2016-01-07 19:30:34 +0800

    this is an unmeaning but meaningful commit. i swear, i won't never do own thing privately.

--- pgs/index.php
+++ pgs/index.php
@@ -1,8 +1,8 @@
 <?php
 //$configs = $_COOKIE['style_cfg'];
-$configs=null;
-if(isset($_COOKIE['style_cfg'])){
-    $configs=json_decode($_COOKIE['style_cfg'],true);
+$configs = null;
+if (isset($_COOKIE['style_cfg'])) {
+    $configs = json_decode($_COOKIE['style_cfg'], true);
 }
 include_once "build/servo/env.php";
 ?>
@@ -40,8 +40,8 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <script src="../bootstrap/js/respond.min.js"></script>
     <![endif]-->
     <script>
-        window.envir= window.envir || {};
-        window.envir.hostConfig= JSON.parse('<?=json_encode(Env::getConfig()) ?>');
+        window.envir = window.envir || {};
+        window.envir.hostConfig = JSON.parse('<?=json_encode(Env::getConfig()) ?>');
     </script>
 </head>
 <!--
@@ -64,7 +64,7 @@ desired effect
 |               | sidebar-mini                            |
 |---------------------------------------------------------|
 -->
-<body class="hold-transition skin-purple sidebar-mini <?= $configs['isCollapsed']?"sidebar-collapse":"" ?> fixed">
+<body class="hold-transition skin-purple sidebar-mini <?= $configs['isCollapsed'] ? "sidebar-collapse" : "" ?> fixed">
 <div class="wrapper">
 
     <!-- Main Header -->
@@ -73,7 +73,7 @@ desired effect
     <aside class="main-sidebar">
 
         <!-- sidebar: style can be found in sidebar.less -->
-         <?php include_once "build/sideMenu.php"; ?>
+        <?php include_once "build/sideMenu.php"; ?>
         <!-- /.sidebar -->
     </aside>
 
@@ -94,6 +94,7 @@ desired effect
         <!-- Main content -->
         <section class="content">
 
+            <div class="fred-col2-auto">
             <!-- Your Page Content Here -->
             <div class="box box-widget fred-note-item">
                 <div class="box-header with-border">
@@ -106,7 +107,8 @@ desired effect
                     <div class="box-tools">
 
                         <button class="btn btn-box-tool"><i class="fa fa-star-o"></i></button>
-                        <button class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
+                        <button class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i>
+                        </button>
                     </div>
                     <!-- /.box-tools -->
                 </div>
@@ -129,7 +131,7 @@ desired effect
 
                         <div class="attachment-pushed">
                             <h4 class="attachment-heading"><a href="http://www.lipsum.com/">Lorem ipsum text
-                                generator</a></h4>
+                                    generator</a></h4>
 
                             <div class="attachment-text">
                                 Description about the attachment can be placed here.
@@ -183,7 +185,8 @@ desired effect
                 </div>
                 <!-- /.box-footer -->
                 <div class="box-footer">
-                    <img class="img-responsive img-circle img-sm fred-img-spacing" src="../dist/img/user4-128x128.jpg"
+                    <img class="img-responsive img-circle img-sm fred-img-spacing"
+                         src="../dist/img/user4-128x128.jpg"
                          alt="alt text">
                     <!-- .img-push is used to add margin to elements next to floating images -->
                     <div class="input-group">
@@ -195,11 +198,10 @@ desired effect
                 </div>
                 <!-- /.box-footer -->
             </div>
-
-
-            <div class="post fred-note-item">
+            <div class="box box-widget fred-note-item">
                 <div class="user-block">
-                    <img class="img-circle img-bordered-sm" src="../dist/img/user1-128x128.jpg" alt="user image">
+                    <img class="img-circle img-bordered-sm" src="../dist/img/user1-128x128.jpg"
+                         alt="user image">
                         <span class="username">
                           <a href="#">Jonathan Burke Jr.</a>
                           <a href="#" class="pull-right btn-box-tool"><i class="fa fa-times"></i></a>
@@ -241,11 +243,12 @@ desired effect
                 </div>
                 <!-- /.row -->
                 <ul class="list-inline">
-                    <li><a href="#" class="link-black text-sm"><i class="fa fa-share margin-r-5"></i> Share</a></li>
+                    <li><a href="#" class="link-black text-sm"><i class="fa fa-share margin-r-5"></i> Share</a>
+                    </li>
                     <li><a href="#" class="link-black text-sm"><i class="fa fa-thumbs-o-up margin-r-5"></i> Like</a>
                     </li>
                     <li class="pull-right"><a href="#" class="link-black text-sm"><i
-                            class="fa fa-comments-o margin-r-5"></i> Comments (5)</a></li>
+                                class="fa fa-comments-o margin-r-5"></i> Comments (5)</a></li>
                 </ul>
                 <div class="box-footer box-comments">
                     <div class="box-comment">
@@ -281,7 +284,8 @@ desired effect
                 </div>
                 <!-- /.box-footer -->
                 <div class="box-footer">
-                    <img class="img-responsive img-circle img-sm fred-img-spacing" src="../dist/img/user4-128x128.jpg"
+                    <img class="img-responsive img-circle img-sm fred-img-spacing"
+                         src="../dist/img/user4-128x128.jpg"
                          alt="alt text">
                     <!-- .img-push is used to add margin to elements next to floating images -->
                     <div class="input-group">
@@ -293,7 +297,106 @@ desired effect
                 </div>
                 <!-- /.box-footer -->
             </div>
+            <div class="box box-widget fred-note-item">
+                <div class="user-block">
+                    <img class="img-circle img-bordered-sm" src="../dist/img/user1-128x128.jpg"
+                         alt="user image">
+                        <span class="username">
+                          <a href="#">Jonathan Burke Jr.</a>
+                          <a href="#" class="pull-right btn-box-tool"><i class="fa fa-times"></i></a>
+                        </span>
+                    <span class="description">Shared publicly - 7:30 PM today</span>
+                </div>
+                <!-- /.user-block -->
+                <p>
+                    Lorem ipsum represents a long-held tradition for designers,
+                    typographers and the like. Some people hate it and argue for
+                    its demise, but others ignore the hate as they create awesome
+                    tools to help create filler text for everyone from bacon lovers
+                    to Charlie Sheen fans.
+                </p>
 
+                <div class="row margin-bottom">
+                    <div class="col-sm-6">
+                        <img class="img-responsive" src="../dist/img/photo1.png" alt="Photo">
+                    </div>
+                    <!-- /.col -->
+                    <div class="col-sm-6">
+                        <div class="row">
+                            <div class="col-sm-6">
+                                <img class="img-responsive" src="../dist/img/photo2.png" alt="Photo">
+                                <br>
+                                <img class="img-responsive" src="../dist/img/photo3.jpg" alt="Photo">
+                            </div>
+                            <!-- /.col -->
+                            <div class="col-sm-6">
+                                <img class="img-responsive" src="../dist/img/photo4.jpg" alt="Photo">
+                                <br>
+                                <img class="img-responsive" src="../dist/img/photo1.png" alt="Photo">
+                            </div>
+                            <!-- /.col -->
+                        </div>
+                        <!-- /.row -->
+                    </div>
+                    <!-- /.col -->
+                </div>
+                <!-- /.row -->
+                <ul class="list-inline">
+                    <li><a href="#" class="link-black text-sm"><i class="fa fa-share margin-r-5"></i> Share</a>
+                    </li>
+                    <li><a href="#" class="link-black text-sm"><i class="fa fa-thumbs-o-up margin-r-5"></i> Like</a>
+                    </li>
+                    <li class="pull-right"><a href="#" class="link-black text-sm"><i
+                                class="fa fa-comments-o margin-r-5"></i> Comments (5)</a></li>
+                </ul>
+                <div class="box-footer box-comments">
+                    <div class="box-comment">
+                        <!-- User image -->
+                        <img class="img-circle img-sm" src="../dist/img/user3-128x128.jpg" alt="user image">
+
+                        <div class="comment-text">
+                      <span class="username">
+                        Maria Gonzales
+                        <span class="text-muted pull-right">8:03 PM Today</span>
+                      </span><!-- /.username -->
+                            It is a long established fact that a reader will be distracted
+                            by the readable content of a page when looking at its layout.
+                        </div>
+                        <!-- /.comment-text -->
+                    </div>
+                    <!-- /.box-comment -->
+                    <div class="box-comment">
+                        <!-- User image -->
+                        <img class="img-circle img-sm" src="../dist/img/user4-128x128.jpg" alt="user image">
+
+                        <div class="comment-text">
+                      <span class="username">
+                        Luna Stark
+                        <span class="text-muted pull-right">8:03 PM Today</span>
+                      </span><!-- /.username -->
+                            It is a long established fact that a reader will be distracted
+                            by the readable content of a page when looking at its layout.
+                        </div>
+                        <!-- /.comment-text -->
+                    </div>
+                    <!-- /.box-comment -->
+                </div>
+                <!-- /.box-footer -->
+                <div class="box-footer">
+                    <img class="img-responsive img-circle img-sm fred-img-spacing"
+                         src="../dist/img/user4-128x128.jpg"
+                         alt="alt text">
+                    <!-- .img-push is used to add margin to elements next to floating images -->
+                    <div class="input-group">
+                        <input type="text" name="message" placeholder="Type Message ..." class="form-control">
+                      <span class="input-group-btn">
+                        <button type="button" class="btn btn-warning btn-flat">Send</button>
+                      </span>
+                    </div>
+                </div>
+                <!-- /.box-footer -->
+            </div>
+            </div>
         </section>
         <!-- /.content -->
     </div>
@@ -402,7 +505,7 @@ desired effect
 <script src="../powerd/scripts/common.js"></script>
 <script>
     $(document).ready(function () {
-        var isPageMax=false;
+        var isPageMax = false;
         var mScrollHeight = 0; //滚动距离总长(注意不是滚动条的长度)
         var mScrollTop = 0;   //滚动到的当前位置
         var mWindowHeight = $(window).height();
@@ -415,7 +518,7 @@ desired effect
                     $(".pull-up-load").css("visibility", "visible");
                     return;
                 }
-                if(isPageMax){
+                if (isPageMax) {
                     if ($(".pull-up-load").css("visibility") == 'visible') {
                         $(".pull-up-load").css("visibility", "hidden");
                     }
